f0385bdba23adda29d69119eec83f750c06630b8
AfianAvro <wahedemon09@gmail.com>
2021-10-04 01:23:02 +0600

    fake data add

---
+++ src/fakeData/fakeData.js
@@ -0,0 +1,111 @@
+const data = [
+  {
+    id: 1,
+    c_img:
+      "https://template.hasthemes.com/maxcoach-v1/maxcoach/assets/images/courses/370/course-1.jpg",
+    course: "old",
+    c_price: "5$",
+    c_name: "Learning to write as a professional author",
+    C_total_lesson: "20 Lessons",
+  },
+  {
+    id: 2,
+    c_img:
+      "https://template.hasthemes.com/maxcoach-v1/maxcoach/assets/images/courses/370/course-2.jpg",
+    course: "new",
+    c_price: "00$ (Free)",
+    c_name: "Customer-centric Info-Tech Strategies",
+    C_total_lesson: "23 Lessons",
+  },
+  {
+    id: 3,
+    c_img:
+      "https://template.hasthemes.com/maxcoach-v1/maxcoach/assets/images/courses/370/course-3.jpg",
+    course: "old",
+    c_price: "19$",
+    c_name: "Open Programming course for Everyone:Python",
+    C_total_lesson: "16 Lessons",
+  },
+  {
+    id: 4,
+    c_img:
+      "https://template.hasthemes.com/maxcoach-v1/maxcoach/assets/images/courses/370/course-4.jpg",
+    course: "new",
+    c_price: "26$",
+    c_name: "Academic Listenung and Note-taking",
+    C_total_lesson: "14 Lessons",
+  },
+  {
+    id: 5,
+    c_img:
+      "https://template.hasthemes.com/maxcoach-v1/maxcoach/assets/images/courses/370/course-5.jpg",
+    course: "old",
+    c_price: "39$",
+    c_name: "Master jQuery in a short Period of time",
+    C_total_lesson: "6 Lessons",
+  },
+  {
+    id: 6,
+    c_img:
+      "https://template.hasthemes.com/maxcoach-v1/maxcoach/assets/images/courses/370/course-6.jpg",
+    course: "new",
+    c_price: "59$",
+    c_name: "Introduction to Javascript for Beginners",
+    C_total_lesson: "14 Lessons",
+  },
+  {
+    id: 7,
+    c_img:
+      "https://template.hasthemes.com/maxcoach-v1/maxcoach/assets/images/courses/370/course-7.jpg",
+    course: "new",
+    c_price: "26$",
+    c_name: "Problem-solving Skills for Better success",
+    C_total_lesson: "15 Lessons",
+  },
+  {
+    id: 8,
+    c_img:
+      "https://template.hasthemes.com/maxcoach-v1/maxcoach/assets/images/courses/370/course-8.jpg",
+    course: "old",
+    c_price: "22$",
+    c_name: "Speed Reading techniques in University",
+    C_total_lesson: "18 Lessons",
+  },
+  {
+    id: 9,
+    c_img:
+      "https://template.hasthemes.com/maxcoach-v1/maxcoach/assets/images/courses/370/course-9.jpg",
+    course: "old",
+    c_price: "22$",
+    c_name: "Database & SQL for Computer Science",
+    C_total_lesson: "15 Lessons",
+  },
+  {
+    id: 10,
+    c_img:
+      "https://template.hasthemes.com/maxcoach-v1/maxcoach/assets/images/courses/370/course-10.jpg",
+    course: "new",
+    c_price: "23$",
+    c_name: "Intermediate English Speking Practice",
+    C_total_lesson: "18 Lessons",
+  },
+  {
+    id: 11,
+    c_img:
+      "https://template.hasthemes.com/maxcoach-v1/maxcoach/assets/images/courses/370/course-11.jpg",
+    course: "old",
+    c_price: "00$ (Free)",
+    c_name: "Complete Fitness Traines: Beginner to Advanced",
+    C_total_lesson: "8 Lessons",
+  },
+  {
+    id: 12,
+    c_img:
+      "https://template.hasthemes.com/maxcoach-v1/maxcoach/assets/images/courses/370/course-12.jpg",
+    course: "new",
+    c_price: "20$",
+    c_name: "Lazy Dancer Ballet Burn -30 Days programme",
+    C_total_lesson: "6 Lessons",
+  }
+];
+console.log(data);
